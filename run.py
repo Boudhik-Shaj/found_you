615a18039c8f6bbbec6415b1fb778bb0261f1302
Boudhik Shaj <82608513+Boudhik-Shaj@users.noreply.github.com>
2023-01-01 23:50:20 +0530

    combined username and details
    have to change print to some variable

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ import os
 import platform
 
 
-os.getlogin() #to get username
-platform.uname() # ('Windows', 'name', 'XP', '5.1.2600', 'x86', 'x86 Family 6 Model 15 Stepping 6, GenuineIntel')
+username=os.getlogin() #to get username
+detail=platform.uname() # ('Windows', 'name', 'XP', '5.1.2600', 'x86', 'x86 Family 6 Model 15 Stepping 6, GenuineIntel')
+print(username+' '+details)
